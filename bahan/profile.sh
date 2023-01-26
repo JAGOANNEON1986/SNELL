660c0f74344e2c6d6b0f0b422c140efd59aa2d63
JAGOANNEON1986 <121962859+JAGOANNEON1986@users.noreply.github.com>
2023-01-26 13:58:17 +0700

    Create profile.sh

---
+++ bahan/profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+neofetch
+echo -e " Selamat datang di layanan AutoScript Snell By JAGOANNEON"
+echo -e " Ketik \e[1;32m nano /etc/snell-server.conf \e[0m untuk menampilkan ulang PORT dan PSK dan merubahnya"
+echo -e ""
